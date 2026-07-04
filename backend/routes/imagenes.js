f1488e044fe855fe4487642c75afb3658bbb715b
wmartinc <mcox4525@gmail.com>
2026-07-03 19:59:25 -0600

    Se elimina, se asignara un repositorio unico

--- backend/routes/imagenes.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const { getImageUrl } = require('../controllers/imagenesController');
-
-const router = Router();
-
-router.get('/image-url', getImageUrl);
-
-module.exports = router;
